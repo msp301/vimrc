03dfb8de7e35280231c3d264c990fe3fcb7819a2
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2023-08-12 11:59:58 +0100

    Create setup.ps1 for Windows machines
    Ansible cannot be run directly on a Windows box (not without relying
    on WSL anyway). Use Powershell to setup a dev environment in
    Windows.

---
+++ setup.ps1
@@ -0,0 +1,40 @@
+function SymlinkDotfile([string] $target, [string] $link) {
+	$file = Get-Item $link -Force -ErrorAction SilentlyContinue
+	$exists = [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
+
+	if(!$exists) {
+		Write-Host "Creating link from '$target' to '$link'"
+		New-Item -ItemType SymbolicLink -Force -Target "$target"  -Path "$link" > $null
+	}
+}
+
+function ChocoInstall([string] $package) {
+    $has_choco = Get-Command -Name choco.exe -ErrorAction SilentlyContinue
+    if(!$has_choco) {
+        # Install Chocolatey
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    }
+
+    $has_package = Get-Command -Name "$package" -ErrorAction SilentlyContinue
+    if(!$has_package) {
+        choco install -y "$package"
+    }
+}
+
+SymlinkDotfile "$PSScriptRoot\init.vim" "$env:LOCALAPPDATA\nvim\init.vim"
+SymlinkDotfile "$PSScriptRoot\ideavimrc" "$env:USERPROFILE\.ideavimrc"
+
+ChocoInstall "fd"
+ChocoInstall "git"
+ChocoInstall "neovide"
+ChocoInstall "python3-virtualenv"
+ChocoInstall "ripgrep"
+
+# Install vim-plug
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force > $null
+
+nvim --headless +PlugInstall +qall
+
+Write-Host "Done"
+
