d1540cf52808fc1e67c309e5de0702032dfc95b5
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2023-08-12 12:44:35 +0100

    Update Mason registries during setup

--- setup.ps1
+++ setup.ps1
@@ -40,6 +40,7 @@ iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
     ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force > $null
 
 nvim --headless +PlugInstall +qall
+nvim --headless +MasonUpdate +qall
 
 Write-Host "Done"
 
