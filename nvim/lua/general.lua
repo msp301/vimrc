6b7d69e336ec845ef0758904863f046432474314
Martin Pritchard <martin@martinpritchard.com>
2024-03-19 20:42:35 +0000

    Neovim Lua linter automatically reformatted lua config files

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -4,57 +4,56 @@ vim.g.have_nerd_font = true
 
 local opt = vim.opt
 
-opt.number         = true
+opt.number = true
 opt.relativenumber = true
 
-local indent       = 4
-opt.tabstop        = indent
-opt.shiftwidth     = indent
-opt.expandtab      = true
-opt.autoindent     = true
-opt.breakindent    = true
-opt.smartindent    = true
-opt.smarttab       = true
+local indent = 4
+opt.tabstop = indent
+opt.shiftwidth = indent
+opt.expandtab = true
+opt.autoindent = true
+opt.breakindent = true
+opt.smartindent = true
+opt.smarttab = true
 
-opt.wildmenu       = true
-opt.wildmode       = "list:longest,full"
-opt.mouse          = "a"
-opt.laststatus     = 2
-opt.signcolumn     = "yes"
-opt.showmatch      = true
-opt.showmode       = false
-opt.spelllang      = "en_gb"
-opt.updatetime     = 250
-opt.timeoutlen     = 300
+opt.wildmenu = true
+opt.wildmode = "list:longest,full"
+opt.mouse = "a"
+opt.laststatus = 2
+opt.signcolumn = "yes"
+opt.showmatch = true
+opt.showmode = false
+opt.spelllang = "en_gb"
+opt.updatetime = 250
+opt.timeoutlen = 300
 
-opt.list           = true
-opt.listchars      = "tab:→ ,"
+opt.list = true
+opt.listchars = { tab = "→ " }
 
 -- Preview substitutions as you type
-opt.inccommand     = "split"
+opt.inccommand = "split"
 
-opt.splitright     = true
-opt.splitbelow     = true
+opt.splitright = true
+opt.splitbelow = true
 
-opt.scrolloff      = 8
+opt.scrolloff = 8
 
 -- Provide access to system clipboard
-opt.clipboard      = "unnamedplus"
+opt.clipboard = "unnamedplus"
 
-opt.backupdir      = "~/.vim-backupsdir"
-opt.dir            = "~/.vim-swapdir"
+opt.backupdir = "~/.vim-backupsdir"
+opt.dir = "~/.vim-swapdir"
 
 -- Search Settings
-opt.incsearch      = true
-opt.ignorecase     = true
-opt.smartcase      = true
-opt.hlsearch       = false
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = false
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
-
