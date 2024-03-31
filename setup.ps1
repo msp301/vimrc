ba706785e8345d2c80abc8648c0578dbc349ed7d
Martin Pritchard <1196474+msp301@users.noreply.github.com>
2024-03-31 12:09:55 +0100

    Add GNU Make to Windows setup

--- setup.ps1
+++ setup.ps1
@@ -1,40 +1,49 @@
-function Symlink([string] $target, [string] $link) {
+function Symlink([string] $target, [string] $link)
+{
     $file = Get-Item $link -Force -ErrorAction SilentlyContinue
     $exists = [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
 
-    if($file.PSIsContainer -and !$exists) {
+    if($file.PSIsContainer -and !$exists)
+    {
         Remove-Item -Recurse -Force -Path $link
     }
 
-    if(!$exists) {
+    if(!$exists)
+    {
         Write-Host "Creating link from '$target' to '$link'"
         New-Item -ItemType SymbolicLink -Force -Target "$target"  -Path "$link" > $null
     }
 }
 
-function SymlinkDirectory([string] $target, [string] $link) {
+function SymlinkDirectory([string] $target, [string] $link)
+{
     $file = Get-Item $link -Force -ErrorAction SilentlyContinue
     $exists = [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
 
-    if(!$exists) {
+    if(!$exists)
+    {
         Write-Host "Creating link from '$target' to '$link'"
         New-Item -ItemType SymbolicLink -Force -Target "$target"  -Path "$link" > $null
     }
 }
 
-function ChocoInstall([string] $package, [string] $executable) {
+function ChocoInstall([string] $package, [string] $executable)
+{
     $has_choco = Get-Command -Name choco.exe -ErrorAction SilentlyContinue
-    if(!$has_choco) {
+    if(!$has_choco)
+    {
         # Install Chocolatey
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     }
 
-    if($executable -eq '') {
+    if($executable -eq '')
+    {
         $executable = $package
     }
 
     $has_package = Get-Command -Name "$executable" -ErrorAction SilentlyContinue
-    if(!$has_package) {
+    if(!$has_package)
+    {
         choco install -y "$package"
     }
 }
@@ -49,6 +58,7 @@ ChocoInstall "fd"
 ChocoInstall "fzf"
 ChocoInstall "git"
 ChocoInstall "golang"
+ChocoInstall "make"
 ChocoInstall "microsoft-windows-terminal" "wt"
 ChocoInstall "neovide"
 ChocoInstall "nerd-fonts-noto"
